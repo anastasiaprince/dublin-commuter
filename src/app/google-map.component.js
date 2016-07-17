bb0c62986d67c7ab6afb53db0494d43c79316681
Anastasia Prince <msanaprince@gmail.com>
2016-07-18 00:41:51 +0100

    Initial commit

---
+++ src/app/google-map.component.js
@@ -0,0 +1,15 @@
+'use strict';
+
+angular
+  .module('googleMap', [])
+  .component('googleMap', {
+    templateUrl: 'app/google-map.template.html',
+    controller: function MyController(NgMap, $http) {
+      var self = this;
+      self.zoom = 11;
+      self.center = '53.349819,-6.2609523';
+      $http.get('https://api.jcdecaux.com/vls/v1/stations?contract=dublin&apiKey=371210d83e4976258d109d993ededcc395bce09e').then(function (response) {
+        self.stations = response.data;
+      });
+    }
+  });
