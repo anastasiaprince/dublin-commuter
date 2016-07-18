dc7dd3ffbc55d1dce16d36644f97627d498b8862
Anastasia Prince <msanaprince@gmail.com>
2016-07-18 01:10:27 +0100

    Change map height to 100%

--- src/app/google-map.component.js
+++ src/app/google-map.component.js
@@ -6,7 +6,7 @@ angular
     templateUrl: 'app/google-map.template.html',
     controller: function MyController(NgMap, $http) {
       var self = this;
-      self.zoom = 11;
+      self.zoom = 13;
       self.center = '53.349819,-6.2609523';
       $http.get('https://api.jcdecaux.com/vls/v1/stations?contract=dublin&apiKey=371210d83e4976258d109d993ededcc395bce09e').then(function (response) {
         self.stations = response.data;
