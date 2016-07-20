90d403d7a9df17c93ffde9849afa334c951d1587
Anastasia Prince <msanaprince@gmail.com>
2016-07-20 14:38:05 +0100

    Add info windows

--- src/app/google-map.component.js
+++ src/app/google-map.component.js
@@ -4,12 +4,19 @@ angular
   .module('googleMap', [])
   .component('googleMap', {
     templateUrl: 'app/google-map.template.html',
-    controller: function MyController(NgMap, $http) {
+    controller: function GetStations(NgMap, $http) {
       var self = this;
+      NgMap.getMap().then(function (map) {
+        self.map = map;
+      });
       self.zoom = 13;
       self.center = '53.349819,-6.2609523';
       $http.get('https://api.jcdecaux.com/vls/v1/stations?contract=dublin&apiKey=371210d83e4976258d109d993ededcc395bce09e').then(function (response) {
         self.stations = response.data;
       });
+      self.showStationInfo = function (event, index) {
+        self.station = self.stations[index];
+        self.map.showInfoWindow('stationInfo', this);
+      };
     }
   });
